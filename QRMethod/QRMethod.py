02a4fc8008aaa5ec79fb573ae83f8b3f3740616a
Julien Monfils <monfils.julien@gmail.com>
2022-12-04 21:01:06 +0100

    QRMethod/QRMethod.py

--- QRMethod/QRMethod.py
+++ QRMethod/QRMethod.py
@@ -1,11 +1,12 @@
 import numpy as np
 from numpy import linalg as lng
 
-def QRMethod(matrix, precision, maxIteration):
+def QRMethod(matrix, precision, maxIteration, ConvAlert = False):
     """
     matrix      : A square matrix (numpy array)
     precision   : Precision used to compute the eigenvectors
     maxIteration: The maximum number of QR iteration done by the algorithm
+    ConvAlert   : if True, the method raise an error when the method does not converge in less than maxIteration iterations
 
     return  : The eigenvalues and eigenvectors of the given matrix
     """
@@ -19,9 +20,17 @@ def QRMethod(matrix, precision, maxIteration):
         Ai = np.dot(R, Q)
 
         iteration += 1
-        print(Ai)
-    return np.diagonal(Ai)
+
+        lowerTri = np.tril(Ai, -1)
+        delta = np.sum(lowerTri)
+
+    if iteration >= maxIteration and ConvAlert:
+        raise Exception("method does not converge in " + str(maxIteration) + " iterations")
+    
+    eigenvalues = np.diagonal(Ai)
+
+    return eigenvalues
 
 A = np.array([[1,2],[2,1]])
 
-print(QRMethod(A, 5, 15))
+print(QRMethod(A, 10**(-5), 15, ConvAlert=True))
